ce58a1ffa0edd7e163f1bf95a627b5a3a4cd9df2
lidiya.lisovskaya <lid.lisovskaya1991@gmail.com>
2025-05-06 17:55:31 +0400

    update of endpoints

--- autotests/src/test/java/api/endpoints/Endpoints.java
+++ autotests/src/test/java/api/endpoints/Endpoints.java
@@ -4,9 +4,10 @@ public class Endpoints {
 
     public static final String BASE_URL = "http://localhost:8000";
     public static final String API = "/api";
-    public static final String REGISTER = "/register";
-    public static final String TOKEN = "/token";
+    public static final String AUTH = "/auth";
+    public static final String REGISTER = AUTH + "/register";
+    public static final String TOKEN = AUTH + "/token";
     public static final String ARTISTS = "/artists/";
     public static final String ALBUMS = "/albums/";
-    public static final String USERS_USERNAME_RIGHTS = "/users/%s/rights";
+    public static final String USERS_USERNAME_RIGHTS = AUTH + "/users/%s/rights";
 }
